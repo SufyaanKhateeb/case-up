cc64a96c7cdbe97f18242f8c5eb2d8f8e6f297b9
Muhammad Sufyaan Khateeb <m.khateeb@castsoftware.com>
2024-07-30 03:40:37 +0530

    add site under construction banner

--- app/page.tsx
+++ app/page.tsx
@@ -47,6 +47,16 @@ export default function Home() {
 					</div>
 				</section>
 			</div>
+			<div className="bg-background p-8 rounded-lg shadow-lg text-center">
+        <h1 className="text-4xl font-bold text-violet-500 mb-4">Site Under Construction</h1>
+        <div className="flex justify-center">
+            <svg className="animate-spin h-10 w-10 text-foreground" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+            </svg>
+        </div>
+        <p className="text-foreground mt-6">In the meantime, you can reach me at <a href="mailto:sufyaankhateeb@gmail.com" className="text-violet-400 underline">sufaankhateeb@gmail.com</a></p>
+			</div>
 			<div className="m-2" />
 			<div ref={workSectionRef} id="work-section" className="w-screen h-screen">
 				Work
